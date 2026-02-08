bff62c04d8176a77e5908fdda6395672d4f14a87
fkrravimax <fkrravimax@users.noreply.github.com>
2026-02-08 11:47:35 +0700

    Update api.ts

--- apps/web/src/services/api.ts
+++ apps/web/src/services/api.ts
@@ -9,7 +9,7 @@ const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
-    withCredentials: true,
+    withCredentials: false,
 });
 
 // Request Interceptor: Attach Token
